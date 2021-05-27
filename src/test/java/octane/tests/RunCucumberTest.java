cc0c3b3e100661f3c5ff619dde25b5092d39c54f
Aldo <aldo.lopes>
2021-05-27 00:07:47 -0300

    Fix Tests story #4001

--- src/test/java/octane/tests/RunCucumberTest.java
+++ src/test/java/octane/tests/RunCucumberTest.java
@@ -1,9 +1,12 @@
 package octane.tests;
 
 import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin={"pretty","summary","com.hpe.alm.octane.OctaneGherkinFormatter:target/gherkin-results/RunnerTest_OctaneGherkinResults.xml"})
 public class RunCucumberTest {
 
 }
