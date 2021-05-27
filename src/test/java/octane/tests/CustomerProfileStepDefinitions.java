ca4cf387301ec46a25878c6f6084fa78235e73de
Aldo <aldo.lopes>
2021-05-26 22:31:52 -0300

    Added Step Automation user story#4001

---
+++ src/test/java/octane/tests/CustomerProfileStepDefinitions.java
@@ -0,0 +1,56 @@
+package octane.tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CustomerProfileStepDefinitions {
+
+    @Given("Navigate to portal")
+    public void navigate_to_portal() {
+        System.out.println("Navigate to portal...");
+    }
+
+    @When("add profile")
+    public void add_profile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user added")
+    public void user_added() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("delete profile")
+    public void delete_profile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user deleted")
+    public void user_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("select option {string}")
+    public void select_option(String string1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("click {int}")
+    public void click(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("option updated")
+    public void option_updated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ src/test/resources/octane/tests/Customer Profile_1001.feature
@@ -0,0 +1,26 @@
+#Auto generated Octane revision tag
+@BSPID1001REV0.2.0
+Feature: Customer Profile
+@TSCID2006
+	Scenario: Add new profile
+		Given Navigate to portal
+		When add profile
+		Then user added
+
+@TSCID2007
+	Scenario: Delete profile
+		Given Navigate to portal
+		When delete profile
+		Then user deleted
+
+@TSCID2008
+	Scenario Outline: Update profile options
+    	Given Navigate to portal
+		When select option <param1> 
+		When click <param2>  
+		Then option updated
+
+	Examples:
+		| param1 | param2 |
+		| a      | 1      |
+		| b      | 2      |
